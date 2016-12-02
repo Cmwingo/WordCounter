1d7c6fec273b456b6346a6b61001fb2a5bac658c
Wingo <cmwingo@gmail.com>
2016-12-02 10:05:36 -0800

    Second test passing

--- Tests/RepeatCounterTest.cs
+++ Tests/RepeatCounterTest.cs
@@ -12,5 +12,12 @@ namespace WordCounter
       RepeatCounter testRepeatCounter = new RepeatCounter("A", "A");
       Assert.Equal(1, testRepeatCounter.CountRepeats());
     }
+
+    [Fact]
+    public void RepeatCounterTest1_Identical_False()
+    {
+      RepeatCounter testRepeatCounter = new RepeatCounter("A", "I");
+      Assert.Equal(0, testRepeatCounter.CountRepeats());
+    }
   }
 }
