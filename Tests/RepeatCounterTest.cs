fbca60c18151c0e049720c247f867ec6a5603c9e
Wingo <cmwingo@gmail.com>
2016-12-02 10:17:11 -0800

    Fourth test passing

--- Tests/RepeatCounterTest.cs
+++ Tests/RepeatCounterTest.cs
@@ -14,7 +14,7 @@ namespace WordCounter
     }
 
     [Fact]
-    public void RepeatCounterTest1_Identical_False()
+    public void RepeatCounterTest1_NotIdentical_True()
     {
       RepeatCounter testRepeatCounter = new RepeatCounter("A", "I");
       Assert.Equal(0, testRepeatCounter.CountRepeats());
@@ -25,6 +25,13 @@ namespace WordCounter
     {
       RepeatCounter testRepeatCounter = new RepeatCounter("A", "A A A");
       Assert.Equal(3, testRepeatCounter.CountRepeats());
+    }
+
+    [Fact]
+    public void RepeatCounterTest_PartialMatches_False()
+    {
+      RepeatCounter testRepeatCounter = new RepeatCounter("ham", "I love ham on hamburgers");
+      Assert.Equal(1, testRepeatCounter.CountRepeats());
     }
   }
 }
